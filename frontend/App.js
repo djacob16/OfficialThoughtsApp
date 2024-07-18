cbeb6827d90e1bfa11fce6ff64ce6857f76d708b
sreenandm <148009692+sreenandm@users.noreply.github.com>
2024-07-18 14:24:45 -0400

    fixed amplify

--- frontend/App.js
+++ frontend/App.js
@@ -3,7 +3,7 @@ import { View } from "react-native";
 import Router from './navigation/Router';
 import Toast from 'react-native-toast-message';
 import { Amplify } from 'aws-amplify';
-import config from '../src/amplifyconfiguration.json';
+import config from './src/amplifyconfiguration.json';
 Amplify.configure(config);
 
 export default function App() {
