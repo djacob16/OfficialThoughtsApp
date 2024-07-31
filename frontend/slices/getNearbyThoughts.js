6597579f30ce3fd80388b43513238303e1553539
sreenandm <148009692+sreenandm@users.noreply.github.com>
2024-07-31 11:58:44 -0400

    uitls

--- frontend/slices/getNearbyThoughts.js
+++ frontend/slices/getNearbyThoughts.js
@@ -1,5 +1,5 @@
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
-import { listNearbyThoughtsWithAuthor } from "../src/graphql/customQueries";
+import { listNearbyThoughtsWithAuthor } from "../utils/customQueries";
 import { listThoughts } from "../src/graphql/queries";
 import { generateClient } from "aws-amplify/api";
 import geohash from "ngeohash";
