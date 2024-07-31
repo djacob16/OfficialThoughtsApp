c887fd4b8974d82c9265c04ae8c8ed25e421eab9
sreenandm <148009692+sreenandm@users.noreply.github.com>
2024-07-31 12:02:32 -0400

    test

--- frontend/data/likeThought.js
+++ frontend/data/likeThought.js
@@ -36,7 +36,7 @@ export const likeThought = async (thoughtID, like) => {
                     }
                 }
             }))
-            console.log("updated liked thought: ", updatedThought)
+            console.log("updated liked thought response: ", updatedThought)
             console.log("like Entry: ", likeEntry)
         } catch (error) {
             console.log("error here: ", error)
