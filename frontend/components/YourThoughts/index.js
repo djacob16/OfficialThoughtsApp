1aac0be2e1bb0ed4f7a17a9bf725b28ad290d32f
jordandeeds31 <33381472+jordandeeds31@users.noreply.github.com>
2024-08-11 11:58:56 -0400

    clean up

--- frontend/components/YourThoughts/index.js
+++ frontend/components/YourThoughts/index.js
@@ -1,9 +1,6 @@
-import React, { useState, useEffect } from 'react';
-import { View, Text, ScrollView, TouchableOpacity, TextInput, Image, Button } from 'react-native';
+import React, { useEffect } from 'react';
+import { View, Text } from 'react-native';
 import styles from "./styles";
-import heart from '../../assets/heartIcon.png';
-import arrowDown from '../../assets/arrowDown.png';
-import Toast from 'react-native-toast-message';
 import { useDispatch, useSelector } from "react-redux";
 import YourActiveThought from '../YourActiveThought';
 import { getActiveThoughts } from '../../slices/getActiveThoughts';
